f4397ec24ef1c56e18916477d5306bc94d2dbdd8
Buzz <jure.eres42@gmail.com>
2022-06-04 23:55:15 +0200

    1.0.0
    No back end but ui is done, i think

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from './components/AddTask';
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState (false)
   const [tasks, setTasks] = useState(
     [
         {
@@ -28,6 +29,9 @@ function App() {
 
 //Add task
 const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
   console.log(task)
 }
 
@@ -50,8 +54,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : 'No tasks to show'}
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types'
 
 
-const Button = ({color, text, onCLick}) => {
+const Button = ({color, text, onClick}) => {
   return (
-  <button style={{backgroundColor: color}} className="btn">{text}</button>)
+  <button 
+      onClick={onClick} 
+      style={{backgroundColor: color}} 
+      className="btn"
+  >
+    {text}
+  </button>)
 }
 
 Button.defaultProps = {

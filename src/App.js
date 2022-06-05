0e0c47b5260733b55ca3b9b11bf7cf9bd6d2ee1c
Buzz <jure.eres42@gmail.com>
2022-06-05 13:53:02 +0200

    1.2.0
    done, working app with backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
-import Header from "./components/Header";
-import Footer from './components/Footer';
-import Tasks from "./components/Tasks";
-import AddTask from './components/AddTask';
-import About from './components/About';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Header from "./components/Header"
+import Footer from './components/Footer'
+import Tasks from "./components/Tasks"
+import AddTask from './components/AddTask'
+import About from './components/About'
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -97,25 +97,35 @@ const App = () => {
 
   return (
     <Router>
-      <div className="container">
+      <div className='container'>
         <Header
           onAdd={() => setShowAddTask(!showAddTask)}
-          showAdd={showAddTask} />
-
-        {showAddTask && <AddTask onAdd={addTask} />}
-
-        {tasks.length > 0 ? (
-          <Tasks
-            tasks={tasks}
-            onDelete={deleteTask}
-            onToggle={toggleReminder}
-          />) : 'No tasks to show'}
-        <Route path='/about' component={About} />
+          showAdd={showAddTask}
+        />
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  'No Tasks To Show'
+                )}
+              </>
+            }
+          />
+          <Route path='/about' element={<About />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
-
-  );
+  )
 }
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
+import { Link } from 'react-router-dom'
+
 const Footer = () => {
   return (
     <footer>
         <p>Copyright &copy; 2022</p>
-        <a href="/about">About</a>
+        <Link to='/about'>About</Link>
     </footer>
   )
 }

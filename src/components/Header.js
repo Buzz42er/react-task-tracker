3fd58db6f262671ba7e1e4ca815d68e233c7d5e4
Buzz <jure.eres42@gmail.com>
2022-08-13 18:04:33 +0200

    1.4.0

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      {location.pathname === '/' && (
+      {location.pathname === '/react-task-tracker' && (
       <Button
         color={showAdd ? 'red' : 'green'}
         text={showAdd ? 'Close' : 'Add'}
